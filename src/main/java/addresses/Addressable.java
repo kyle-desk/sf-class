68e6f51383d96fd1235e863429aaf2cb6c810a47
Simon Roberts <simon@dancingcloudservices.com>
2015-05-07 16:11:56 -0700

    Added pdf slide decks for lambdas and static/default interface methods Added default method for Addressable.getAddressLabel() Illustrated List.forEach and Consumer<T> interface

--- src/main/java/addresses/Addressable.java
+++ src/main/java/addresses/Addressable.java
@@ -4,4 +4,9 @@ public interface Addressable {
 	String getStreet();
 	String getCity();
 	int getZip();
+	default String getAddressLabel() {
+		return getStreet() + "\n"
+			+ getCity() + "\n"
+			+ getZip();
+	}
 }
--- src/main/java/businesspartners/TestCustomers.java
+++ src/main/java/businesspartners/TestCustomers.java
@@ -1,7 +1,11 @@
 package businesspartners;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+
+import addresses.Addressable;
 
 public class TestCustomers {
 
@@ -9,6 +13,12 @@ public class TestCustomers {
 		List<Customer> lc = Arrays.asList(
 				new Customer("123 Acacia", "Warwick", 12345, 123),
 				new Customer("1 Market", "San Francisco", 94105, 34),
+				new Customer("27 Market", "San Francisco", 94105, 3400),
+				new Customer("36 Market", "San Francisco", 94105, 934),
+				new Customer("44 Market", "San Francisco", 94105, 374),
+				new Customer("128 Market", "San Francisco", 94105, 534),
+				new Customer("19 Market", "San Francisco", 94105, 3),
+				new Customer("36 Market", "San Francisco", 94105, 304),
 				new Customer("37 Downtown Lane", "Hillsboro", 42971, 97)
 			);
 		System.out.println("     Unsorted: " + lc);
@@ -17,6 +27,30 @@ public class TestCustomers {
 		lc.sort(Customer.getSpendComparator());
 		System.out.println("Sorted by spend: " + lc);
 		
+		lc.sort(/*new Comparator<Addressable>() {
+			@Override
+			public int compare*/(Addressable o1, Addressable o2) -> {
+				return o1.getStreet().compareTo(o2.getStreet());
+			}
+		/*}*/);
+
+		lc.sort((Addressable o1, Addressable o2) -> {
+					return o1.getStreet().compareTo(o2.getStreet());
+				}
+			);
+
+		lc.sort((o1, o2) -> o1.getStreet().compareTo(o2.getStreet()));
+		System.out.println("Sorted by street: " + lc);
+
+		lc.sort((Comparator<Addressable>)((o1, o2) -> o1.getStreet().compareTo(o2.getStreet())));
+		System.out.println("Sorted by street: " + lc);
+		
+		lc.forEach(c-> System.out.println(">" + c.getAddressLabel()));
+		
+		Predicate<String> ps = s -> s.length() > 4;
+		System.out.println("ps is a " + ps.getClass().getName());
+		System.out.println("ps is" + (ps instanceof Predicate ? "":" not") + " a Predicate");
+
 	}
 
 }
